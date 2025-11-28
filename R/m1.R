bc4d343338f464d64eb61476534bc7165358283f
SebastianKvist99 <sebastiantoffnerkvist@gmail.com>
2025-11-28 09:48:30 +0100

    Initial commit

---
+++ R/m1.R
@@ -0,0 +1,18 @@
+#' M1 test for consitence
+#'
+#' @param dataset A data set with item scores and covariates
+#' @param y A character vector
+#'
+#' @returns A Boolean indicating whether the dataset is consistent wrt the M1
+#' criteria
+#' @export
+#'
+#' @examples
+#' df <- data
+#' y <- c("item1", "item2", "item3")
+#' M1(df, y)
+M1 <- function(dataset, y = vector_with_names_of_items){
+  corr_matrix <- cor(dataset[y])
+  fulfilled <- sum(corr_matrix<=0)==0
+  return(fulfilled)
+}
